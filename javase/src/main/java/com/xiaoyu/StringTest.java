5d91d427e0f2901c0356e3f95e6448e6faccc38b
why <378214220@qq.com>
2019-11-04 17:48:50 +0800

    ceshi

--- javase/src/main/java/com/xiaoyu/StringTest.java
+++ javase/src/main/java/com/xiaoyu/StringTest.java
@@ -1,9 +1,15 @@
 package com.xiaoyu;
 
+import java.util.Optional;
+
 /**
  * @创建人 wanghaoyu
  * @创建时间 2019/11/4
- * @描述
+ * @描述 字符串常用方法练习
  */
 public class StringTest {
+    //构造方法,防止空指针异常
+    public static void main(String[] args) {
+
+    }
 }
